5d85208de78b33f35cbcd33ab74fdc81899f0103
lvincent <leonelexequielvincentgmail.com>
2025-11-21 19:39:49 -0300

    refactor: keep App.vue minimal as layout host

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,6 @@ import AppShell from './components/layout/AppShell.vue'
 
 <template>
   <AppShell>
-    <template #header-actions>
-      <span class="text-xs text-slate-500">Construido con Vue 3 + Tailwind</span>
-    </template>
-
     <RouterView />
-
-    <template #footer-extra>
-      <span class="text-xs text-slate-500">v1.0</span>
-    </template>
   </AppShell>
 </template>
